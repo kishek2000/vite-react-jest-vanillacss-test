68ae513989a6452305576ebe8f2228f2bfce2ff3
kishek2000 <kishek12@gmail.com>
2022-01-08 20:28:10 +1100

    feat: attempting fix with moduleNameMapper change

--- vite-project/jest.config.js
+++ vite-project/jest.config.js
@@ -1,1 +1,11 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+
+module.exports = {
+  verbose: true,
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less|css.ts|css.js)$": "<rootDir>/__mocks__/styleMock.js",
+  },
+};
